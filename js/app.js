ea2070ab47c8f0ccd4dc01dffe6424bda72304dd
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-01-15 22:01:23 -0700

    added slide in detail box to project cards

--- js/app.js
+++ js/app.js
@@ -12,16 +12,37 @@ function pageLink() {
 }
 
 function toggleOpen() {
+    console.log(this)
+    const title = this.children[0];
+    const titleSmall = this.children[1];
+    const details = this.children[2];
+    const source = this.children[3];
+
     if(this.classList.contains('open')) {
-        projects.forEach(project => project.classList.remove('open'));
+        title.classList.toggle('active');
+        source.classList.toggle('active');
+        titleSmall.classList.toggle('shrink');
+        details.classList.toggle('slide-in');
+        this.classList.toggle('open');
     }
     else {
-        projects.forEach(project => project.classList.remove('open'));
+        projects.forEach(project => {
+            project.classList.remove('open');
+            project.children[1].classList.remove('shrink');
+            project.children[2].classList.remove('slide-in');
+            project.children[0].classList.remove('active');
+            project.children[3].classList.remove('active');
+        });
         this.classList.toggle('open');
+        title.classList.toggle('active');
+        source.classList.toggle('active');
+        titleSmall.classList.toggle('shrink');
+        details.classList.toggle('slide-in');
     }
 }
 
 function toggleActive(e) {
+    console.log(e);
     if(e.propertyName.includes('flex-grow')) {
         this.classList.toggle('open-active');
     }
